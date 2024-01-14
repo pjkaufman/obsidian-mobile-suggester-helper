cc869b8c3e884c428c32d8dacc05838237991bef
Peter Kaufman <PeterJamesKaufman@gmail.com>
2024-01-14 16:55:35 -0500

    first attempt at the plugin is not quite right and needs some work

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'google',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    '@typescript-eslint',
+    'unicorn',
+  ],
+  'rules': {
+    'camelcase': 'off',
+    'max-len': 'off',
+    'no-constant-binary-expression': 'error',
+    'no-template-curly-in-string': 'error',
+    'no-unmodified-loop-condition': 'error',
+    'no-unreachable-loop': 'error',
+    'no-unused-private-class-members': 'error',
+    'require-jsdoc': 'off',
+    'unicorn/template-indent': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        'argsIgnorePattern': '(^_)|(options)',
+      },
+    ],
+  },
+};

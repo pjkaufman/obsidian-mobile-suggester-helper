333e94bfc1f9c48f86b6785fcde046141b066fb1
Peter Kaufman <PeterJamesKaufman@gmail.com>
2024-01-14 22:03:58 -0500

    got some of the file generation working, but there are at least two scenarios that do not work properly

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import {Plugin} from 'obsidian';
+import {Editor, MarkdownView, Plugin, normalizePath} from 'obsidian';
 
 const downArrowKeyEvent = new KeyboardEvent('keydown', {
   which: 40,
@@ -29,6 +29,41 @@ export default class MobileSuggesterHelper extends Plugin {
         this.dispatchEventIfSuggesterExists(upArrowKeyEvent);
       },
     });
+
+    this.addCommand({
+      id: 'mobile-suggester-helper-select-heading',
+      name: 'Select the highlighted item in the suggester.',
+      icon: 'check-in-circle',
+      callback: () => {
+        const editor = this.getEditor();
+        if (!editor) {
+          return;
+        }
+
+        const position = editor.getCursor();
+        if (!position) {
+          return;
+        }
+
+        const line = editor.getLine(position.line);
+        let linkStart = line.indexOf("[[");
+        if (linkStart === -1) {
+          return;
+        }
+
+        linkStart += 2;
+
+        const stringPath = this.getPathForSelectedSuggesterItem()
+        if (!stringPath) {
+          return;
+        }
+
+        editor.replaceRange(stringPath, {
+          line: position.line,
+          ch: linkStart,
+        }, position);
+      },
+    });
   }
 
   onunload() {}
@@ -39,4 +74,35 @@ export default class MobileSuggesterHelper extends Plugin {
       suggesterDiv.dispatchEvent(event);
     }
   }
+
+  getPathForSelectedSuggesterItem(): string | null {
+    const selectedNoteTitle = document.querySelector('body > div.suggestion-container > div.suggestion > div.suggestion-item.mod-complex.is-selected > div.suggestion-content > div.suggestion-title > span');
+    if (!selectedNoteTitle) {
+      return null;
+    }
+
+    const selectedNotePath = document.querySelector('body > div.suggestion-container > div.suggestion > div.suggestion-item.mod-complex.is-selected > div.suggestion-content > div.suggestion-note');
+
+    let pathToFind = selectedNoteTitle.textContent;
+    if (selectedNotePath) {
+      const notePath = selectedNotePath.textContent;
+      if (notePath?.endsWith('/')) {
+        pathToFind = normalizePath(notePath+pathToFind);
+      } else {
+        pathToFind = notePath;
+      }
+    }
+
+    return pathToFind ;
+  }
+
+    /**
+   * Gets the current markdown editor if it exists {@link https://github.com/chrisgrieser/obsidian-smarter-paste/blob/master/main.ts#L37-L41|Obsidian Smarter Paste Source}
+   * @return {Editor} Returns the current codemirror editor if there is an active view of type markdown or null if there is not one.
+   */
+    private getEditor(): Editor | null {
+      const activeLeaf = this.app.workspace.getActiveViewOfType(MarkdownView);
+      if (!activeLeaf) return null;
+      return activeLeaf.editor;
+    }
 }
